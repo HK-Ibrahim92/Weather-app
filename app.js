a1f063b7876ddd59bd3b6523e8304c3186f67c76
Syed Ibrahim <Syedzada786alam@gmail.com>
2023-09-04 01:12:10 +0500

    UI changes

--- app.js
+++ app.js
@@ -1,76 +1,80 @@
 
 // var city=prompt("enter city")
-var input=document.getElementById("search")
- var info=document.getElementById("line1")
- var info2=document.getElementById("line2")
- var info3=document.getElementById("line3")
- var info4=document.getElementById("line4")
+var input = document.getElementById("search")
+var info = document.getElementById("line1")
+var info2 = document.getElementById("line2")
+var info3 = document.getElementById("line3")
+var info4 = document.getElementById("line4")
+var info5=document.getElementById("line5")
+var city = document.getElementById("city")
+var center = document.getElementById("center")
+var date = document.getElementById("date")
 
- var city=document.getElementById("city")
- var center=document.getElementById("center")
- 
- var date=document.getElementById("date")
- let weather;
-
-
- function updatetime(){
+let weather;
+function updatetime() {
   const currentDate = new Date();
-  date.innerHTML=currentDate.toLocaleTimeString();
-  
- }
-
- updatetime();
-setInterval(updatetime, 1000);
-
-async function nio(cityName){fetch
-    (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=dc18faa214c79784c865b45687feb174&units=metric`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-
-   info.innerHTML=(data.main.temp+"℃")
-   info2.innerHTML=(data.weather[0].main)
-   info3.innerHTML=("Feel :"+data.main.feels_like)
-   info4.innerHTML=("Wind:"+data.wind.speed+"km/h") 
-let upper=cityName.toUpperCase()
-  city.innerHTML=(upper)
-  weather=info2.innerHTML
-  console.log(weather)
-
-  
-  check();
+  date.innerHTML = currentDate.toLocaleTimeString();
 
 }
-)
 
- .catch(error=> console.log("Internet connection lost"))
- }
+updatetime();
+setInterval(updatetime, 1000);
+
+async function nio(cityName) {
+  fetch
+    (`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=dc18faa214c79784c865b45687feb174&units=metric`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+
+      info.innerHTML = (data.main.temp + "℃")
+      info5.innerHTML = ("Humidity :" + data.main.humidity + "%")
+      info2.innerHTML = (data.weather[0].main)
+      info3.innerHTML = ("Feel :" + data.main.feels_like + "℃")
+      
+      info4.innerHTML = ("Wind:" + data.wind.speed + "km/h")
+      let upper = cityName.toUpperCase()
+      city.innerHTML = (upper)
+      weather = info2.innerHTML
+      console.log(weather)
+      console.log(data.main.temp + "℃")
+      check();
+
+    }
+    )
+    .catch(error => swal.fire({
+      title: "Error!",
+      text: "City not Found.",
+      icon: "error",
+      button: "OK",
+    }))
+}
 
 
- window.onload = function() {
+window.onload = function () {
   nio("Karachi"); // Display default Karachi weather on page load
 };
 
 function handleSearch() {
   var userInput = input.value.trim();
   if (userInput) {
-    nio(userInput); 
+    nio(userInput);
     input.value = "";
   }
 }
 
-input.addEventListener("keydown", function(event) {
+input.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     handleSearch();
   }
 });
 
- async function check() {
+async function check() {
   try {
     // weather = await nio()
     if (!weather) {
-      
-    }else{
+
+    } else {
       if (weather === "Rain") {
         document.body.style.backgroundImage = "url('./image/rain.jpg')";
         console.log("It's raining!");
@@ -78,31 +82,31 @@ input.addEventListener("keydown", function(event) {
       } else if (weather === "Drizzle") {
         document.body.style.backgroundImage = "url('./image/drizzle.jpg')";
         // Set background for drizzle
-      } 
+      }
       else if (weather === "Thunderstorm") {
         document.body.style.backgroundImage = "url('./image/thunderstorm.jpg')";
         // Set background for thunderstorm
       }
-       else if (weather === "Snow") {
+      else if (weather === "Snow") {
         document.body.style.backgroundImage = "url('./image/snow.jpg')";
         // Set background for snow
       }
-       else if (weather === "Clear") {
+      else if (weather === "Clear") {
         document.body.style.backgroundImage = "url('./image/clearsky.jpg')";
         // Set background for clear weather
       }
-       else if (weather === "Clouds") {
+      else if (weather === "Clouds") {
         document.body.style.backgroundImage = "url('./image/cloud.jpg')";
         // Set background for clouds
       }
-       else if (weather === "Haze" || weather === "Mist" || weather === "Fog" || weather === "Smoke") {
+      else if (weather === "Haze" || weather === "Mist" || weather === "Fog" || weather === "Smoke") {
         document.body.style.backgroundImage = "url('./image/smoke.jpg')";
-         console.log("It's not raining.");
-       
+        console.log("It's not raining.");
+
       } else {
         // Set default background for other cases
       }
-      
+
 
 
     }
